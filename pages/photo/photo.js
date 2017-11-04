e32ba157809cffb4cd3b45525121e3d32285d0ea
ly <15820480843@163.com>
2017-11-04 15:22:59 +0800

    接口完善

--- pages/photo/photo.js
+++ pages/photo/photo.js
@@ -1,22 +1,31 @@
 // pages/login/login.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     cameraContext:{},
-    identName: "刘阳",
-    ident: "421222198910262830",
+    identName: "",
+    ident: "",
+    recognizeIcon:"../../images/icon/recognize_loding.png",
+    middleText:"识别中···",
+    phoneText:"开始拍照",
+    tipState:false,
+    buttonState:true,
+    isDim:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(wx.canIUse('camera'))
-   
-  
+    this.setData({
+      identName: options.identName,
+      ident: options.ident
+    })
+    console.log("拍照启动")
+    console.log(options.identName)
+    console.log(options.ident)
   },
 
   /**
@@ -39,43 +48,97 @@ Page({
   },
 
   startTakePhoto: function () {
+
     var that = this;
+    that.setData({
+      tipState:true,
+      buttonState:false,
+      recognizeIcon: "../../images/icon/recognize_loding.png",
+      middleText: "识别中···",
+    })
     console.log(123456);
     that.cameraContext.takePhoto({
-      quality:"normal",
+      quality:"high",
       success: function (res) {
-        console.log(res.tempImagePath);
-        that.getBase64Image(res.tempImagePath);
-        // that.setData({
-        //   // src: res.tempImagePath 
-        // })
+        that.upload(res.tempImagePath);
+        that.setData({
+          isDim: true
+        })
       },
       fail: function () {
-        wx.showLoading({
-          title: "2",
-        })
+        //失败需要统一还原状态
       },
      });
   
   },
 
+  upload: function (filePath) {
+    var that = this;  
+    wx.uploadFile({
+      url: "https://dev.bookingyun.com/CenterMaster/police/checkIdentAngPic",
+      filePath: filePath,
+      name: 'file',
+      formData:
+      {
+        ident: that.data.ident, identName: that.data.identName
+      },
+      success: function (res) {
+        console.log(res);
+        console.log(res.data);
+        var dataObject = JSON.parse(res.data);
+        console.log(dataObject.returnCode);
+       
+        if (dataObject.returnCode == 1) {
+          
+          that.setData({
+            recognizeIcon: "../../images/icon/recognize_suc.png",
+            middleText: "实名登记成功",
+          })
+          setTimeout(function () {
+            wx.navigateBack({
+              delta: 1
+            })
+          }
+            , 2000)
 
-  // 转base64
-  getBase64Image:function(img) {  
-    // var canvas = document.createElement("canvas");  
-    // canvas.width = img.width;  
-    // canvas.height = img.height;  
-    // var ctx = canvas.getContext("2d");  
-    // ctx.drawImage(img, 0, 0, img.width, img.height);  
-    // var ext = img.src.substring(img.src.lastIndexOf(".") + 1).toLowerCase();  
-    // var dataURL = canvas.toDataURL("image/" + ext);  
+        } else {
+          that.setData({
+            buttonState: true,
+            recognizeIcon: "../../images/icon/recognize_fail.png",
+            phoneText: "再试一次",
+            middleText: "人像识别未通过",
+            isDim: false
+          })
+        }
+      },
+      fail: function (error) {
+        console.error(error);
+      }
+    })
 
-    // that.identRecognize(that.data.identName, that.data.ident, dataURL);
-    // wx.base64ToArrayBuffer(base64)
 
-    console.log(222);
-    var base64 = wx.arrayBufferToBase64(img)
-    console.log(base64);
+    //上传图片到服务器
+    wx.uploadFile({
+      url: "https://dev.bookingyun.com/KmsMaster/hotel/addHotelImg",
+      filePath: filePath,
+      name: 'file',
+      formData: 
+      {
+        hotelId: "B335C79F2B7748A49DCF962BDBC8D220", itemType: "401"
+      },
+      success: function (res) {
+        console.log("上传");
+        console.log(res);
+        console.log(res.data.returnCode);
+      },
+      fail: function (error) {
+        console.error(error);
+        wx.showToast({
+          title: error.daga.returnMessage,
+          icon: "loading"
+        })
+      }
+    })
   },
 
 
